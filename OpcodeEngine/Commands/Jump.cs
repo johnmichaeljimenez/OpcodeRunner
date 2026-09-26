23a3b5407c8741b7734edb4cbcf50dab7740dc9f
John <johnmichaeljimenez.thecmd@gmail.com>
2026-09-26 17:00:22 +0800

    Added JumpEQ, JumpNEQ, JumpGT, JumpLT

--- OpcodeEngine/Commands/Jump.cs
+++ OpcodeEngine/Commands/Jump.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using OpcodeEngine.Core;
 
 namespace OpcodeEngine.Commands
@@ -33,5 +34,83 @@ namespace OpcodeEngine.Commands
 			if (boolValue)
 				Instruction.Jump(labelName);
 		}
+	}
+
+	public abstract class JumpCompare : Command
+	{
+		[CommandParameter]
+		private string left = "";
+
+		[CommandParameter]
+		private string right = "";
+
+		[CommandParameter]
+		private string labelName = "";
+
+		protected abstract bool Compare(int comparisonResult);
+
+		public override void OnEnter()
+		{
+			var a = ResolveOperand(left);
+			var b = ResolveOperand(right);
+
+			if (Compare(CompareValues(a, b)))
+				Instruction.Jump(labelName);
+		}
+
+		private object ResolveOperand(string operand)
+		{
+			var var = Engine.GetVar(operand);
+			return var != null ? var.Value : operand;
+		}
+
+		private static int CompareValues(object a, object b)
+		{
+			if (TryGetNumber(a, out var x) && TryGetNumber(b, out var y))
+				return x.CompareTo(y);
+
+			return string.Compare(
+				Convert.ToString(a, CultureInfo.InvariantCulture),
+				Convert.ToString(b, CultureInfo.InvariantCulture),
+				StringComparison.OrdinalIgnoreCase);
+		}
+
+		private static bool TryGetNumber(object value, out float number)
+		{
+			if (value is int i)
+			{
+				number = i;
+				return true;
+			}
+
+			if (value is float f)
+			{
+				number = f;
+				return true;
+			}
+
+			return float.TryParse(Convert.ToString(value, CultureInfo.InvariantCulture),
+				NumberStyles.Float, CultureInfo.InvariantCulture, out number);
+		}
+	}
+
+	public class JumpEQ : JumpCompare
+	{
+		protected override bool Compare(int comparisonResult) => comparisonResult == 0;
+	}
+
+	public class JumpNEQ : JumpCompare
+	{
+		protected override bool Compare(int comparisonResult) => comparisonResult != 0;
+	}
+
+	public class JumpGT : JumpCompare
+	{
+		protected override bool Compare(int comparisonResult) => comparisonResult > 0;
+	}
+
+	public class JumpLT : JumpCompare
+	{
+		protected override bool Compare(int comparisonResult) => comparisonResult < 0;
 	}
 }
